45fb2c7b2f1f2a4cf39c5e359a5a4492274d3bec
Ondrej Sindelar <sindelar.ondra@gmail.com>
2026-03-19 18:17:49 +0100

    aktualizace

---
+++ android/app/src/main/java/com/sindelarondra/oxidy/MainActivity.java
@@ -0,0 +1,22 @@
+package com.sindelarondra.oxidy;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = findViewById(R.id.webview);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+}
